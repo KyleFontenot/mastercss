58d4f58e7da1e66b526331120e88a61b5a37a3a5
KyleFontenot <kylewilliamfontenot@gmail.com>
2025-02-09 23:10:05 -0800

    Multiple bundle handling

--- build.ts
+++ build.ts
@@ -8,14 +8,41 @@ const modules = [
 ]
 
 try {
-  for (const module of modules) {
-    const build = await Bun.build({
-      entrypoints: [`./src/${module}/src/index.ts`],
-      outdir: `./dist/${module}`,
-      target: 'node'
+    const esmbuild = await Bun.build({
+      entrypoints: [
+        `./src/core/src/index.ts`,
+        `./src/extractor/src/index.ts`,
+        `./src/runtime/src/index.ts`,
+        `./src/validator/src/index.ts`,
+      ],
+      outdir: `./dist/`,
+      format: 'esm',
+      target: 'bun',
+      splitting: true,
     } satisfies BuildOptions)
-    console.log(build)
-  }
+
+    const cjsbuild = await Bun.build({
+      entrypoints: [
+        `./src/core/src/index.ts`,
+        `./src/extractor/src/index.ts`,
+        `./src/runtime/src/index.ts`,
+        `./src/validator/src/index.ts`,
+      ],
+      outdir: `./dist/`,
+      format: 'cjs',
+      target: 'node',
+      splitting: true,
+    } satisfies BuildOptions)
+
+  //for (const module of modules) {
+  //  const build = await Bun.build({
+  //    entrypoints: [`./src/${module}/src/index.ts`],
+  //    outdir: `./dist/${module}`,
+  //    target: 'node',
+  //    splitting: true,
+  //  } satisfies BuildOptions)
+  //  console.log(build)
+  //}
 }
 catch (e) {
   console.error(e)
